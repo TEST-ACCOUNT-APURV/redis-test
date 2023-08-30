35898b361af547d14f5078330279d90c298c041a
Mathieu Benoit <mathieu-benoit@hotmail.fr>
2023-08-30 18:34:55 -0400

    github_actions_organization_secret

--- terraform/github_secrets.tf
+++ terraform/github_secrets.tf
@@ -17,20 +17,20 @@ resource "github_actions_organization_secret" "humanitec_token" {
 resource "github_actions_organization_secret" "container_registry_name" {
   count             = var.humanitec_env_type == "development" ? 1 : 0
   visibility        = "all"
-  secret_name       = "CONTAINER_REGISTRY_NAME"
+  secret_name       = "GCP_GAR_NAME"
   plaintext_value   = "${google_artifact_registry_repository.gar_containers.location}-docker.pkg.dev/${var.gcp_project_id}/${google_artifact_registry_repository.gar_containers.name}"
 }
 
 resource "github_actions_organization_secret" "container_registry_host" {
   count             = var.humanitec_env_type == "development" ? 1 : 0
   visibility        = "all"
-  secret_name       = "CONTAINER_REGISTRY_HOST"
+  secret_name       = "GCP_GAR_HOST"
   plaintext_value   = "${google_artifact_registry_repository.gar_containers.location}-docker.pkg.dev"
 }
 
 resource "github_actions_organization_secret" "container_registry_writer_key" {
   count             = var.humanitec_env_type == "development" ? 1 : 0
   visibility        = "all"
-  secret_name       = "CONTAINER_REGISTRY_WRITER_KEY"
+  secret_name       = "GCP_GAR_WRITER_KEY"
   plaintext_value   = base64decode(google_service_account_key.gar_writer_access_key.private_key)
 }
