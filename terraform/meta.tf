95d149111e11d4ff49abfc44adbc966e211a5f26
Mathieu Benoit <mathieu-benoit@hotmail.fr>
2023-09-14 03:41:26 -0400

    cleanup

--- terraform/meta.tf
+++ terraform/meta.tf
@@ -5,11 +5,3 @@ data "google_client_config" "default" {}
 data "http" "icanhazip" {
    url = "http://icanhazip.com"
 }
-
-data "kubernetes_service" "ingress_nginx_controller" {
-  metadata {
-    name      = "ingress-nginx-controller"
-    namespace = "ingress-nginx"
-  }
-  depends_on = [helm_release.ingress_nginx]
-}
