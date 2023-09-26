d6ae3004fee9c4d89b0a24eb365c3b57179706f4
Mathieu Benoit <mathieu-benoit@hotmail.fr>
2023-09-26 01:19:35 -0700

    doc

--- resources/gke-existing/gsa.tf
+++ resources/gke-existing/gsa.tf
@@ -10,6 +10,7 @@ resource "google_project_iam_member" "gke_admin" {
   member  = "serviceAccount:${google_service_account.gke_cluster_access.email}"
 }
 
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_service_account_key#private_key
 resource "google_service_account_key" "gke_cluster_access_key" {
   service_account_id = google_service_account.gke_cluster_access.name
 }
