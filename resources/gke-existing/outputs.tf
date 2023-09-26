a93165c9f663483d321dd5a86b801781af3c8921
Mathieu Benoit <mathieu-benoit@hotmail.fr>
2023-09-25 22:51:27 -0700

    jsonencode credentials

--- resources/gke-existing/outputs.tf
+++ resources/gke-existing/outputs.tf
@@ -19,6 +19,6 @@ output "zone" {
 }
 
 output "credentials" {
-  value = base64decode(google_service_account_key.gke_cluster_access_key.private_key)
+  value = jsonencode(base64decode(google_service_account_key.gke_cluster_access_key.private_key))
   sensitive = true
 }
