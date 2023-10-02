b532196e570f4f5126afbb77d23fc4d3ef408088
Mathieu Benoit <mathieu-benoit@hotmail.fr>
2023-10-02 13:38:58 -0400

    name = "redis-${random_string.memorystore_instance_name.result}"

--- resources/memorystore-new/main.tf
+++ resources/memorystore-new/main.tf
@@ -7,7 +7,7 @@ resource "random_string" "memorystore_instance_name" {
 
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/redis_instance
 resource "google_redis_instance" "memorystore" {
-  name                = random_string.memorystore_instance_name.result
+  name                = "redis-${random_string.memorystore_instance_name.result}"
   memory_size_gb      = 1
   redis_version       = "REDIS_6_X"
   region              = var.region
