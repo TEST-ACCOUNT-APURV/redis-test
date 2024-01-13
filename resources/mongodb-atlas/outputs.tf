de038fbfe149b275d60d15a8582e64f2c43cfe18
Mathieu Benoit <mathieu.benoit@humanitec.com>
2024-01-13 17:18:57 +0000

    Fix mongodb connection in output

--- resources/mongodb-atlas/outputs.tf
+++ resources/mongodb-atlas/outputs.tf
@@ -1,4 +1,4 @@
 output "connection" {
-  value     = replace(mongodbatlas_cluster.cluster.connection_strings[0].standard_srv, "mongodb+srv://", format("mongodb+srv://%s:%s@%s", mongodbatlas_database_user.user.username, mongodbatlas_database_user.user.password, trimprefix(mongodbatlas_cluster.cluster.connection_strings[0].standard_srv, "mongodb+srv://")))
+  value     = replace(mongodbatlas_cluster.cluster.connection_strings[0].standard_srv, "mongodb+srv://", format("mongodb+srv://%s:%s@", mongodbatlas_database_user.user.username, mongodbatlas_database_user.user.password))
   sensitive = true
 }
