a7cb4b63c337898b85c77eba8e0d54d8b9cbc9e9
Mathieu Benoit <mathieu.benoit@humanitec.com>
2024-01-13 17:14:29 +0000

    Fix connection with mongodb user/password

--- resources/mongodb-atlas/outputs.tf
+++ resources/mongodb-atlas/outputs.tf
@@ -1,4 +1,4 @@
 output "connection" {
-  value     = mongodbatlas_cluster.cluster.connection_strings
+  value     = replace(mongodbatlas_cluster.cluster.connection_strings[0].standard_srv, "mongodb+srv://", format("mongodb+srv://%s:%s@%s", mongodbatlas_database_user.user.username, mongodbatlas_database_user.user.password, trimprefix(mongodbatlas_cluster.cluster.connection_strings[0].standard_srv, "mongodb+srv://")))
   sensitive = true
 }
