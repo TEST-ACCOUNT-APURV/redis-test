3b1009f81681165e08a093be2f33275d07f8dcd2
Mathieu Benoit <mathieu-benoit@hotmail.fr>
2024-01-15 01:48:31 -0500

    GSA's email output non-sensitive

--- resources/gsa/outputs.tf
+++ resources/gsa/outputs.tf
@@ -1,3 +1,3 @@
 output "email" {
-  value     = nonsensitive(var.iam_members == null || length(var.iam_members.resource_names) == 0 ? "" : google_service_account.gsa[0].email)
+  value     = var.iam_members == null || length(var.iam_members.resource_names) == 0 ? "" : google_service_account.gsa[0].email
 }
